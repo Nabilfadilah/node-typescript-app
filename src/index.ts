7174257dc66fcbea280bcfd44cfd3a85ea592fd9
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-19 10:08:11 +0700

    Menambahkan route default

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import mongoose from "mongoose"
+import router from "./routes";
 
 const app = express()
 app.use(express.json())
@@ -12,6 +13,8 @@ mongoose.connect(MONGO_URL, {
     console.log("Database connected");
 })
 .catch((error) => console.log(error))
+
+app.use("/", router);
 
 app.listen(4000, () => {
     console.log(`Server running on http://localhost:4000`)
